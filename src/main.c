dbc303918f3df131eab3e318473d5f9dcf6c7663
Joris OFFOUGA <offougajoris@gmail.com>
2022-11-04 15:18:33 +0100

    build multi file with inc directory
    Signed-off-by: Joris OFFOUGA <offougajoris@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "version.h"
 
 //
 // main is where all program execution starts
 //
 int main(int argc, char **argv)
 {
-  printf("Hello world.\n");
+  printf("Hello world from version %s.\n", get_version());
   return 0;
 }
---
+++ src/version.c
@@ -0,0 +1,6 @@
+#include "version.h"
+
+char* get_version()
+{
+    return "1.0.0";
+}
